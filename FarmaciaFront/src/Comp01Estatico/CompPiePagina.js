9a4e3f90722d8486a8a14f1684cea5839940566d
JacobC2001 <jcaychocanamero@gmail.com>
2022-03-27 12:14:55 -0500

    first commit

---
+++ FarmaciaFront/src/Comp01Estatico/CompPiePagina.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class CompPiePagina extends Component {
+    render() {
+        return (
+            <nav className="navbar fixed-bottom navbar-expand-sm navbar dark bg-dark">
+                <div className="container">
+                    <span className="navbar brand text-white fs-5" href="#">MEGAFARMA</span>
+                    <span className="text-secondary">&copy;2022</span>
+                </div>
+            </nav>
+        );
+    }
+}
+export default CompPiePagina;
